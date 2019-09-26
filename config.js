bc5c2a0b3f12d719a81299737197df14d304892b
PawelKonczalski <50076223+PawelKonczalski@users.noreply.github.com>
2019-09-26 13:27:28 +0200

    admin login panel

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+ keySession: ['tasiemiec'],
+    maxAgeSession: 24 * 60 * 60 * 1000,
+};
--- routes/admin.js
+++ routes/admin.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
+router.all('*', (req, res, next) => {
+    if (!req.session.admin) {
+        res.redirect('/login');
+        return;
+    }
+    next();
+});
+
 router.get('/', (req, res) => {
-    res.render('admin', { title: 'Admin' });
+    res.render('admin', {title: 'Admin'});
 });
 
 module.exports = router;
